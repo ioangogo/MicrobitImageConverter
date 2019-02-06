1211c8b87b627d4f12da2788962949c8930ebef7
Ioan Loosley <legit.ioangogo@gmail.com>
2019-02-06 17:29:35 +0000

    rename and other housecleaning tasks

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(name='mBitImageConverter',
+      version='1.0',
+      description='Converts any image format understood by PILLOW into a MicroBitImage,
+      author='Ioan Loosley',
+      author_email='legit.ioangogo@gmail.com',
+      url='https://github.com/ioangogo/MicrobitImageConverter',
+      packages=['mBitImageConverter'],
+     )
